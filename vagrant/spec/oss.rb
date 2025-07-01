e30445d4d133c8028c46f8ec7f33084d355d9aaa
Feng Ruohang <rh@vonng.com>
2025-07-01 18:16:04 +0800

    use arm virtualbox compatible debian12 image

--- vagrant/spec/oss.rb
+++ vagrant/spec/oss.rb
@@ -1,14 +1,14 @@
-# oss: pigsty building environment templates: RockyLinux 9.x / Debian 12.x / Ubuntu 22.04.x
+# pro: pigsty 5-node PRO building environment templates
 
 Specs = [
 
   # RockyLinux 9.5
   { "name" => "el9",    "ip" => "10.10.10.9" ,  "cpu" => "1",  "mem" => "2048",  "image" =>  "bento/rockylinux-9"     },
 
-  # Debian 12.9
-  { "name" => "d12",    "ip" => "10.10.10.12",  "cpu" => "1",  "mem" => "2048",  "image" =>  "generic/debian12"       },
+  # Debian 12.x
+  { "name" => "d12",    "ip" => "10.10.10.12",  "cpu" => "1",  "mem" => "2048",  "image" =>  "cloud-image/debian-12"  },
 
-  # Ubuntu 22.04.3
-  { "name" => "u22",    "ip" => "10.10.10.22",  "cpu" => "1",  "mem" => "2048",  "image" =>  "generic/ubuntu2204"     },
+  # Ubuntu 24.04.2
+  { "name" => "u24",    "ip" => "10.10.10.24",  "cpu" => "1",  "mem" => "2048",  "image" =>  "bento/ubuntu-24.04"     },
 
 ]
