0c174a794f8b3bfec55273c8c29fb3fac26900d2
Feng Ruohang <rh@vonng.com>
2025-12-08 21:51:17 +0800

    feat: collect etcd log with vector

--- roles/etcd/templates/etcdctl.sh
+++ roles/etcd/templates/etcdctl.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 alias e="etcdctl"
 alias em="etcdctl member"
-export ETCDCTL_ENDPOINTS="{% for ip in groups['etcd']|sort %}{% if not loop.first %},{% endif %}https://{{ ip }}:{{ etcd_port }}{% endfor %}"
+export ETCDCTL_ENDPOINTS="{% for ip in groups[etcd_cluster]|sort %}{% if not loop.first %},{% endif %}https://{{ ip }}:{{ etcd_port }}{% endfor %}"
+{% if inventory_hostname in groups['infra']|default([]) %}
 export ETCDCTL_CACERT=/etc/pki/ca.crt
-{% if inventory_hostname in groups['infra'] %}
 export ETCDCTL_CERT=/etc/pki/infra.crt
 export ETCDCTL_KEY=/etc/pki/infra.key
 {% else %}
+export ETCDCTL_CACERT=/etc/etcd/ca.crt
 export ETCDCTL_CERT=/etc/etcd/server.crt
 export ETCDCTL_KEY=/etc/etcd/server.key
 {% endif %}
