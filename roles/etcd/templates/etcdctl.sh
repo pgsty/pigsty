26bb46e31725225ef06926275959463d0f57c161
Feng Ruohang <rh@vonng.com>
2025-12-18 10:50:40 +0800

    feat: harden etcdctl env and root password

--- roles/etcd/templates/etcdctl.sh
+++ roles/etcd/templates/etcdctl.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 alias e="etcdctl"
 alias em="etcdctl member"
-export ETCDCTL_ENDPOINTS="{% for ip in groups[etcd_cluster]|sort %}{% if not loop.first %},{% endif %}https://{{ ip }}:{{ etcd_port }}{% endfor %}"
-{% if inventory_hostname in groups['infra']|default([]) %}
-export ETCDCTL_CACERT=/etc/pki/ca.crt
-export ETCDCTL_CERT=/etc/pki/infra.crt
-export ETCDCTL_KEY=/etc/pki/infra.key
-{% else %}
+export ETCDCTL_ENDPOINTS="{{ etcdctl_endpoints }}"
+{% if inventory_hostname in groups['etcd']|default([]) %}
 export ETCDCTL_CACERT=/etc/etcd/ca.crt
 export ETCDCTL_CERT=/etc/etcd/server.crt
 export ETCDCTL_KEY=/etc/etcd/server.key
 {% endif %}
+
+case "$(id -un)" in
+  root{{ '|' + node_admin_username if node_admin_enabled and node_admin_username != '' and node_admin_username != 'root' else '' }})
+    [ -r /etc/etcd/etcd.pass ] && export ETCDCTL_USER="root:$(cat /etc/etcd/etcd.pass)"
+    ;;
+esac
